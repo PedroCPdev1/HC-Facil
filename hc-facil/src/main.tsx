cf8a57efff09e161a3c9dd7d1e4688eeaf468610
pedrocpdev1 <pedrocpdev@gmail.com>
2025-09-16 10:37:34 -0300

    Organização dos arquivos

--- hc-facil/src/main.tsx
+++ hc-facil/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import Header from './Header.tsx'
+import './css/index.css'
+import App from './typeScript/App.tsx'
+import Header from './typeScript/Header.tsx'
+import Footer from './typeScript/Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
     <App />
+    <Footer />
   </StrictMode>,
 )
---
+++ hc-facil/src/typeScript/App.tsx
@@ -0,0 +1,12 @@
+import "../css/App.css";
+
+export default function App() {
+  return (
+    <main className="flex flex-col items-center text-center min-h-screen p-4">
+      <div>
+        <h1 className="mt-5 mb-4 text-5xl text-white font-bold tracking-tight">Bem Vindo ao HC Fácil!</h1>
+      </div>
+      <h2 className="text-white font-bold text-3xl">Pressione a opção desejada</h2>
+    </main>
+  );
+}
