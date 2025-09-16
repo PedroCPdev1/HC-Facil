7e02f60093ded2073c082e43b11062e328afd2de
alissonccavalcante <alissonoli4br@gmail.com>
2025-09-16 11:31:49 -0300

    subindo o código

--- hc-facil/src/typeScript/Header.tsx
+++ hc-facil/src/typeScript/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="header bg-white w-full h-30 shadow-xl">
+    <header className="header bg-white w-full h-30 shadow-xl flex fixed">
         <div className="header-container m-0 p-0 w-full h-30 flex flex-row gap-5">
             <img src="../public/logo.png" alt="Logo HC Fácil"/>
         </div>
