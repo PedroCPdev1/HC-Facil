062d8fd5a416decb59306ee2d1e649c797b7c79e
pedrocpdev1 <pedrocpdev@gmail.com>
2025-09-16 10:10:50 -0300

    Implementação do Header

---
+++ hc-facil/src/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header bg-white w-full h-30 shadow-xl">
+        <div className="header-container m-0 p-0 w-full h-30 flex flex-row gap-5">
+            <img src="../public/logo.png" alt="Logo HC Fácil"/>
+        </div>
+    </header>
+  )
+}
