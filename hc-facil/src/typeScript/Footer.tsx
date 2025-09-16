42887b53772bbcfa2524faf14e2b17d1b88c87f6
alissonccavalcante <alissonoli4br@gmail.com>
2025-09-16 11:31:44 -0300

    Criação do footer

---
+++ hc-facil/src/typeScript/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+    <footer className="footer bg-white text-center  text-black p-4">
+        <p>&copy; 2025 Hc Fácil - Todos os direitos reservados</p>
+        </footer>
+    )
+}
