0308c3fb1ce46db0ddf857081e1a4bec61f6d09f
pedrocpdev1 <pedrocpdev@gmail.com>
2025-09-24 21:50:29 -0300

    alteracoea no contato

--- hc-facil/src/routes/Contato/index.tsx
+++ hc-facil/src/routes/Contato/index.tsx
@@ -1,3 +1,22 @@
 export default function Contato() {
-  return <h1>Contato</h1>;
+  return (
+    <main className="h-screen flex flex-col items-center">
+      <div className="p-8 pt-20 text-center ">
+        <h1 className="text-white text-5xl font-bold mb-2">
+          Contato
+        </h1>
+      </div>
+      <div className="grid grid-cols-3 w-500 h-200 p-8 gap-20 bg-white shadow-md rounded-4xl text-center">
+        <div className="flex flex-col p-8 bg-gray-100 rounded-4xl text-3xl">
+          <h2 className="font-bold mb-4">Entre em contato conosco</h2>
+        </div>
+        <div className="flex flex-col p-8 bg-gray-100 rounded-4xl text-3xl">
+          <h2 className="font-bold mb-4">Nossos canais de atendimento</h2>
+        </div>
+        <div className="flex flex-col p-8 bg-gray-100 rounded-4xl text-3xl">
+          <h2 className=" font-bold mb-4">Localização</h2>
+        </div>
+      </div>
+      </main>
+  )
 }
